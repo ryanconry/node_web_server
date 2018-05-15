04f42b0e480e11b66077143ab551b4fb83edc868
ryanconry <r.conry@hotmail.com>
2018-05-14 22:52:15 -0400

    implementing handlebars package

--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 const express = require('express');
+const hbs = require('hbs');
+
+//"nodemon server.js -e js,hbs" to watch all js and hbs file extensions in project
 
 var app = express();
 
-app.get('/', (req,res) => {
-  // res.send('<h1>Hello Express</h1>');
-  res.send({
-    name: 'Ryan',
-    age: 27,
-    city: 'Hamilton'
+hbs.registerPartials(__dirname + '/views/partials');  //set directory for handlebar partials
+app.set('view engine', 'hbs');                    //set view engine to use handlebars
+app.use(express.static(__dirname + '/public'));   //static takes absolute path to what you want to serve up
+                                                   //dirname is path to node_web_server
+
+hbs.registerHelper('getCurrentYear',() => {         //initialize handlebar helper
+  return new Date().getFullYear();
+});
+
+app.get('/', (req,res) => {                        //set behaviour for root page
+  res.render('home.hbs',{                         //render home.hbs on root page
+    pageTitle: 'Home',                            //pass object to render in home template
+    message: 'Welcome to my web server!'
   });
 });
 
 app.get('/about',(req,res)=> {
-  res.send('About Page');
+  res.render('about.hbs',{
+    pageTitle: 'About Page',
+  });
 });
 
 app.get('/bad', (req,res) => {
@@ -22,4 +34,6 @@ app.get('/bad', (req,res) => {
   });
 })
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
