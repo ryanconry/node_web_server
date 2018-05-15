e430e0bbcbfd19a3c53e974d36842db219025137
Ryan Conry <r.conry@hotmail.com>
2018-05-15 11:26:31 -0400

    implementing express middleware (logger and maintenance page)

--- server.js
+++ server.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs')
 
 //"nodemon server.js -e js,hbs" to watch all js and hbs file extensions in project
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');  //set directory for handlebar partials
+                                                    //dirname is absolute path to node_web_server
 app.set('view engine', 'hbs');                    //set view engine to use handlebars
+
+app.use((req,res,next)=> {                        //set up logger middleware
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;   //log entry
+  console.log(log);                               //print to console
+  fs.appendFile('server.log', log + '\n', (err) => {  //save to file
+    console.log('Unable to append to server.log');
+  });
+  next();                                         //call next to return
+})
+
+app.use((req,res,next) => {                       //set up maintenance middleware
+  res.render('maintenance.hbs');
+  next();
+});
+
 app.use(express.static(__dirname + '/public'));   //static takes absolute path to what you want to serve up
-                                                   //dirname is path to node_web_server
 
 hbs.registerHelper('getCurrentYear',() => {         //initialize handlebar helper
   return new Date().getFullYear();
